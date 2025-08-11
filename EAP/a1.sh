ad009d69154c36e6b73fd1748fb55539133c3ee0
Procheta Sen <prochetasen@Prochetas-MacBook-Pro.local>
2025-08-11 16:51:22 +0100

    updated

---
+++ EAP/a1.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -l
+# Use the current working directory and current environment for this job.
+#SBATCH -D ./
+#SBATCH --export=ALL
+
+##SBATCH -o eapMT_tatoeba%j.out
+#SBATCH -o attention_change_llama_coqa%j.out
+##SBATCH -o layerwise_embedding_yelp%j.out
+
+# Define job name
+##SBATCH -J twitter
+
+
+# Request 40 cores on 1 node
+#SBATCH --gres=gpu:1
+#SBATCH -p gpu-a100-lowbig
+#SBTACH -N 1
+#SBATCH -n 8
+# Load the necessary modules
+
+module load miniforge3/25.3.0-python3.12.10
+source activate principle
+export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
+
+for ((i=0; i<23; i++))
+do
+	python EAP_Qwen2.py  $i 
+done
